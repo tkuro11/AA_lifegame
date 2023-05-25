2786c20efd1a8810fbd9fd444c88bc04be3e596b
Takayuki Kurosawa <tkuro11@gmail.com>
2023-05-25 16:33:41 +0900

    prototype convengence function done!
    store board's fingerprint history, and convergence will be detected
    by checking cycles in fingerprint history,
    currently fingerprint is just sum of live point.

--- lifegame.c
+++ lifegame.c
@@ -10,15 +10,17 @@ const int H = 40;
 #define N 16      // the size of hash buffer (MUSTBE even)
 const int MAX_STRIDE = N/2;
 
+void _clear() { printf("\033[2J"); }
+void _home() { printf("\033[0;0f"); }
 
-int sum_surrounding(char board[H][W], int x, int y) {
+int _sum_surrounding(char board[H][W], int x, int y) {
     int sum = 0;
     int d[8][2]= { {-1,-1}, {0, -1}, {1, -1},
                    {-1, 0},          {1,  0},
                    {-1, 1}, {0,  1}, {1,  1} };
 
     for (int i= 0; i < 8; i++) {
-        int lx = x + d[i][0], ly = y + d[i][1];
+        int lx = x+d[i][0], ly = y+d[i][1];
         if (0 <= lx && lx < W &&
             0 <= ly && ly < H && board[ly][lx] == 1) {
             sum += 1;
@@ -28,14 +30,12 @@ int sum_surrounding(char board[H][W], int x, int y) {
     return sum;
 }
 
-void clear() { printf("\033[2J"); }
-void home() { printf("\033[0;0f"); }
 
 void _count_board(char board[H][W], char ret[H][W])
 {
     for (int y = 0; y< H; y++) {
         for (int x = 0; x< W; x++) {
-            ret[y][x] = sum_surrounding(board, x, y);
+            ret[y][x] = _sum_surrounding(board, x, y);
         }
     }
 }
@@ -53,6 +53,32 @@ long _hash_board(char board[H][W])
     }
     return ret;
     
+}
+
+bool _check_stride(unsigned int ary[], int stride)
+{
+    for (int j = 0; j < stride; j++) {
+        int elem = ary[j];
+        for (int i = j+stride; i < N; i+=stride) {
+            if (elem != ary[i]) return false;
+        }
+    }
+    return true;
+}
+
+bool check_converged(char board[H][W])
+{
+    static unsigned int hash[N];
+    static int hash_ptr = 0;
+    bool ret = true;
+
+    hash[hash_ptr] = _hash_board(board);
+    hash_ptr = (hash_ptr + 1) % N;
+
+    for (int s = 1; s< MAX_STRIDE; s++) {
+        if (_check_stride(hash, s)) return true;
+    }
+    return false;
 }
 void initialize_board(char board[H][W])
 {
@@ -80,14 +106,9 @@ void display_board(char board[H][W])
 
 void refresh_board(char board[H][W])
 {
-    char c, sum_mtx[H][W];
-    int flag = 0;
+    char  sum_mtx[H][W];
 
-    for (int y = 0; y< H; y++) {
-        for (int x = 0; x< W; x++) {
-            sum_mtx[y][x] = sum_surrounding(board, x, y);
-        }
-    }
+    _count_board(board, sum_mtx); // at first, make a counting matrix.
     for (int y = 0; y< H; y++) {
         for (int x = 0; x< W; x++) {
             int sum = sum_mtx[y][x];
@@ -107,23 +128,24 @@ void refresh_board(char board[H][W])
 int main(void)
 {
     char board[H][W];
-    int generation = 0;
+    int generation = 1;
     srand((unsigned int) time(NULL));
 
-    clear();
+    _clear();
     initialize_board(board);
     while (true) {
-        home();
-        printf("GEN=%d\n", ++generation);
+        _home();
+        printf("GEN=%5d\n", generation);
         display_board(board);
         refresh_board(board);
-        if (generation > 300) {
-            printf("\nreset scene\n");
+        usleep(100*1000);
+
+        if (++generation > 5000 || check_converged(board)) {
+            printf("\n*** CONVERGED!! ***\n");
             initialize_board(board);
             generation = 1;
             sleep(2);
-            clear();
+            _clear();
         }
-        usleep(100*1000);
     }
 }
